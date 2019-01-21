5cde382384008145a0a4791766f33443e34fe666
CHENS <macinberry@icloud.com>
2019-01-21 23:12:51 +0800

    14小节练习
    第一周的内容就结束啦！多多练习才能掌握！

---
+++ 14-切片.py
@@ -0,0 +1,8 @@
+# 切片是Python的高级特性
+
+L = ['Michael', 'Sarah', 'Tracy', 'Bob', 'Jack'] #L为一个list
+
+print(L[:]) 	#冒号前后都省略，则表示切取首尾
+print(L[:2])	#list正数第一位为0
+print(L[-2:-1])	#list倒数第一位为-1，且冒号后面为开始切的地方
+print("这就是切片")
